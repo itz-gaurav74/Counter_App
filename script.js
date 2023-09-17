e6010705e3fb11078be049602a9014fb8518ee64
itz-gaurav74 <116722012+itz-gaurav74@users.noreply.github.com>
2023-09-17 17:25:32 +0000

    Modifying Js file

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 let dispMsg = document.getElementById('disp_ele');
+let lastCount = document.getElementById('saved_data');
 let count = 0;
 
 function increament() {
   count = count + 1;
-  dispMsg.innerText = count;
-  // console.log(count);
+  dispMsg.textContent = count;
 }
+function decreament() {
+   count -= 1;
+  dispMsg.textContent = count
+}
+function save() {
+  let saveData = count + "-" ;
+  lastCount.textContent += saveData; 
+  count = 0;
+  dispMsg.innerText = 0; 
+}
+
